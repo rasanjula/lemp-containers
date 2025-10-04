025914a630b964da9f830ca06cb2d6ed4ed0d178
rasanjula <rasanjula89@gmail.com>
2025-10-04 23:05:37 +0300

    ci: trigger run

--- db/init/init.sql
+++ db/init/init.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE IF NOT EXISTS appdb;
-CREATE USER IF NOT EXISTS 'appuser'@'%' IDENTIFIED BY 'Rasanjla';  -- must match /opt/lemp/.env on VM
+CREATE USER IF NOT EXISTS 'appuser'@'%' IDENTIFIED BY 'Ra9Dhura';  -- must match /opt/lemp/.env on VM
 GRANT ALL PRIVILEGES ON appdb.* TO 'appuser'@'%';
 FLUSH PRIVILEGES;
